81c9d98e0071d3aa9af2e491dd65bfa79c23d9b8
qkey17 <qkeymail17@gmail.com>
2026-03-09 16:28:05 +0500

    update

--- twitch_api/__init__.py
+++ twitch_api/__init__.py
@@ -2,4 +2,11 @@ from .client import gql_post_json, get_client_id
 from .meta import VodMeta, render_message, fetch_vod_meta
 from .fetch_page import gql_fetch_page
 from .fetch_comments import gql_fetch_comments
-from .emotes import fetch_7tv_emote_map, download_as_data_uri
+from .emotes import (
+    fetch_7tv_emote_map,
+    fetch_bttv_emote_map,
+    fetch_ffz_emote_map,
+    fetch_twitch_global_emote_map,
+    fetch_twitch_channel_emote_map,
+    download_as_data_uri,
+)
