5c7ff7187a22a88ee0462c684f55a4faeaff4caf
qkey17 <qkeymail17@gmail.com>
2026-03-23 01:31:46 +0500

    update

--- src/twitch_api/fetch_comments.py
+++ src/twitch_api/fetch_comments.py
@@ -56,7 +56,6 @@ async def gql_fetch_comments(
             badges = extract_user_badges(node)
             reply = extract_reply_meta(node)
 
-            print("REPLY RAW:", node.get("message", {}).get("reply"))
             if text:
                 yield (offset, created_at, display, text, fragments, color, badges, reply)
 
