a7e78f916639dbf9090a397b57f04b17ed95065c
qkey17 <qkeymail17@gmail.com>
2026-03-22 20:36:38 +0500

    update

--- src/twitch_api/badges.py
+++ src/twitch_api/badges.py
@@ -1,8 +1,12 @@
 import aiohttp
 
+HEADERS = {
+    "Client-ID": "kimne78kx3ncx6brgo4mv6wki5h1ko",
+}
+
 
 async def fetch_twitch_global_badge_map(session: aiohttp.ClientSession):
-    url = "https://badges.twitch.tv/v1/badges/global/display"
+    url = "https://api.twitch.tv/helix/chat/badges/global"
 
     async with session.get(url) as resp:
         data = await resp.json()
@@ -18,16 +22,17 @@ async def fetch_twitch_global_badge_map(session: aiohttp.ClientSession):
 
 
 async def fetch_twitch_channel_badge_map(session: aiohttp.ClientSession, channel_id: str):
-    url = f"https://badges.twitch.tv/v1/badges/channels/{channel_id}/display"
+    url = f"https://api.twitch.tv/helix/chat/badges?broadcaster_id={channel_id}"
 
-    async with session.get(url) as resp:
+    async with session.get(url, headers=HEADERS) as resp:
         data = await resp.json()
 
-    result = {}
+        result = {}
 
-    for badge_name, badge in data.get("badge_sets", {}).items():
-        for version, info in badge.get("versions", {}).items():
-            key = f"{badge_name}/{version}"
-            result[key] = info.get("image_url_2x") or info.get("image_url_1x")
+        for badge in data.get("data", []):
+            set_id = badge.get("set_id")
+            for version in badge.get("versions", []):
+                key = f"{set_id}/{version.get('id')}"
+                result[key] = version.get("image_url_2x") or version.get("image_url_1x")
 
     return result
